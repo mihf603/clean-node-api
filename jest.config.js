3486b81174aaf7e6ab4b6e36eca7c1d40c0c506d
Camilla Fernandes <mihf603@gmail.com>
2023-02-11 19:40:06 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: false,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
