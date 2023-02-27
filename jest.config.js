db645c45b821ec8b5a45889b00f8a447a42d7fb7
Camilla Fernandes <mihf603@gmail.com>
2023-02-27 00:53:13 -0300

    chore: ignore main layer coverage in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: false,
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coveragePathIgnorePatterns: [
+    'protocols'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
